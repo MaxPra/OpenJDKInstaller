66452a01b8c8f037c2de43f07de105f0b86f692f
MaxPra <epiccmax@gmx.at>
2020-10-01 13:07:59 +0200

    Added SourceCode

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace OpenJDKInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tfInstallationPath = new System.Windows.Forms.TextBox();
+            this.btnSelectPath = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tfZipFolderPath = new System.Windows.Forms.TextBox();
+            this.btnSelectOpenJDK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tfInstallationPath
+            // 
+            this.tfInstallationPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tfInstallationPath.Location = new System.Drawing.Point(26, 106);
+            this.tfInstallationPath.Name = "tfInstallationPath";
+            this.tfInstallationPath.Size = new System.Drawing.Size(295, 24);
+            this.tfInstallationPath.TabIndex = 2;
+            // 
+            // btnSelectPath
+            // 
+            this.btnSelectPath.Location = new System.Drawing.Point(341, 106);
+            this.btnSelectPath.Name = "btnSelectPath";
+            this.btnSelectPath.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectPath.TabIndex = 3;
+            this.btnSelectPath.Text = "select";
+            this.btnSelectPath.UseVisualStyleBackColor = true;
+            this.btnSelectPath.Click += new System.EventHandler(this.btnSelectPath_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select the installation path:";
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Location = new System.Drawing.Point(201, 158);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(75, 23);
+            this.btnInstall.TabIndex = 5;
+            this.btnInstall.Text = "Install";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Your zipfolder path:";
+            // 
+            // tfZipFolderPath
+            // 
+            this.tfZipFolderPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tfZipFolderPath.Location = new System.Drawing.Point(26, 55);
+            this.tfZipFolderPath.Name = "tfZipFolderPath";
+            this.tfZipFolderPath.Size = new System.Drawing.Size(295, 24);
+            this.tfZipFolderPath.TabIndex = 6;
+            // 
+            // btnSelectOpenJDK
+            // 
+            this.btnSelectOpenJDK.Location = new System.Drawing.Point(341, 55);
+            this.btnSelectOpenJDK.Name = "btnSelectOpenJDK";
+            this.btnSelectOpenJDK.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectOpenJDK.TabIndex = 9;
+            this.btnSelectOpenJDK.Text = "select";
+            this.btnSelectOpenJDK.UseVisualStyleBackColor = true;
+            this.btnSelectOpenJDK.Click += new System.EventHandler(this.btnSelectOpenJDK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 213);
+            this.Controls.Add(this.btnSelectOpenJDK);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tfZipFolderPath);
+            this.Controls.Add(this.btnInstall);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSelectPath);
+            this.Controls.Add(this.tfInstallationPath);
+            this.MaximumSize = new System.Drawing.Size(476, 252);
+            this.MinimumSize = new System.Drawing.Size(476, 252);
+            this.Name = "Form1";
+            this.Text = "OpenJDKInstaller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tfInstallationPath;
+        private System.Windows.Forms.Button btnSelectPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInstall;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tfZipFolderPath;
+        private System.Windows.Forms.Button btnSelectOpenJDK;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.Linq;
+
+namespace OpenJDKInstaller
+{
+    public partial class Form1 : Form
+    {
+
+        private FileInfo zipFolder = null;
+        private string zipName = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            if (files.Length == 1)
+            {
+                if (files[0].EndsWith(".zip"))
+                {
+                    zipFolder = new FileInfo(files[0]);
+                    tfZipFolderPath.Text = zipFolder.FullName;
+                }
+                else
+                {
+                    MessageBox.Show("Not a ZIPFOLDER!", "No zipfolder here...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Only one file is accepted!", "Too many files!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSelectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrEmpty(fbd.SelectedPath))
+            {
+                tfInstallationPath.Text = fbd.SelectedPath;
+            }
+            
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            if(!tfInstallationPath.Text.Equals("") && !tfZipFolderPath.Equals(""))
+            {
+                try
+                {
+                    ZipFile.ExtractToDirectory(tfZipFolderPath.Text, tfInstallationPath.Text);
+                    var zip = ZipFile.OpenRead(tfZipFolderPath.Text);
+                    string name = zip.Entries.ElementAt(0).ToString();
+                    
+                    MessageBox.Show("JDK was installed successfully!");
+                    
+                    string envPath = Environment.GetEnvironmentVariable("path", EnvironmentVariableTarget.Machine);
+                    if(!envPath.Contains(tfInstallationPath.Text + @"\" + name.Replace('/', '\\') + @"bin"))
+                    {
+                        Environment.SetEnvironmentVariable("path", tfInstallationPath.Text + @"\" + name.Replace('/', '\\') + @"bin;" + envPath, EnvironmentVariableTarget.Machine);
+                    }
+                    
+                    MessageBox.Show("The Environmentvariables were set successfully!");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Something went totally wrong here!\nBe sure, you started this app in administrator mode!");
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("All textfields have to be filled!", "Empty fields!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSelectOpenJDK_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            DialogResult result = fd.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrEmpty(fd.FileName))
+            {
+                if (fd.FileName.EndsWith(".zip"))
+                {
+                    tfZipFolderPath.Text = fd.FileName;
+                }
+                else
+                {
+                    MessageBox.Show("This is not a ZIPFOLDER!", "No zipfolder!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Icon = new System.Drawing.Icon(@"src\icon.ico");
+        }
+    }
+}
